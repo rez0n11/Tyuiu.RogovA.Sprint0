47773144f7b1f1687683fd3a78b45e7e7b05083d
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-15 16:39:40 +0500

    task6

--- Tyuiu.RogovA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.RogovA.Sprint0.Task6.V0/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Tyuiu.RogovA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.RogovA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arraynums = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива:"+DataService.sumArray(arraynums));
+            Console.WriteLine("Разность элементов массива:" + DataService.subArray(arraynums));
+            Console.WriteLine("Произведение элементов массива:" + DataService.multArray(arraynums));
+        }
+    }
+}
